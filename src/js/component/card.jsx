83e880799c8cc3dd268a4ecf83f3d9782ef56530
Suri1336 <146222294+Suri1336@users.noreply.github.com>
2024-02-14 17:39:39 +0000

    not done

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -20,16 +20,68 @@ const Card =(props)=>{
           <p class="card-text">skin color: {props.person.skin_color}</p>
         </div>
         <div class="card-buttons"> 
-        <Link to={'profile/people/'+ props.index} class="btn btn-secondary">🥷</Link>
+        <Link to={'profile/person/'+ props.index} class="btn btn-secondary">🥷</Link>
         <button class="btn btn-secondary" onClick={()=>actions.selectFavorite(props.person.name)}>&#10084;</button>
         </div>
       </div>
     ):null
-
-
-
-
-
+ props.type ==="vehicle"?(
+        field = <div class="card" style="width: 18rem;">
+        <img src={`https://starwars-visualguide.com/assets/img/vehicles/${props.index + 1}.jpg`}
+        onError={(e)=>{
+            e.target.src='https://starwars-visualguide.com/assets/img/placeholder.jpg'
+        }}
+        class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{props.vehicle.name}</h5>
+          <p class="card-text">eye color: {props.vehicle.model}</p>
+          <p class="card-text">gender: {props.vehicle.manufacturer}</p>
+          <p class="card-text">skin color: {props.vehicle.cargo_capacity}</p>
+        </div>
+        <div class="card-buttons"> 
+        <Link to={'profile/vehicle/'+ props.index} class="btn btn-secondary">🥷</Link>
+        <button class="btn btn-secondary" onClick={()=>actions.selectFavorite(props.vehicle.name)}>&#10084;</button>
+        </div>
+      </div>
+    ):null
+    props.type ==="planet"?(
+      field = <div class="card" style="width: 18rem;">
+      <img src={`https://starwars-visualguide.com/assets/img/planets/${props.index + 1}.jpg`}
+      onError={(e)=>{
+          e.target.src='https://starwars-visualguide.com/assets/img/placeholder.jpg'
+      }}
+      class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">{props.planet.name}</h5>
+        <p class="card-text">eye color: {props.planet.rotation_period}</p>
+        <p class="card-text">gender: {props.planet.climate}</p>
+        <p class="card-text">skin color: {props.planet.orbital_period}</p>
+      </div>
+      <div class="card-buttons"> 
+      <Link to={'profile/planet/'+ props.index} class="btn btn-secondary">🥷</Link>
+      <button class="btn btn-secondary" onClick={()=>actions.selectFavorite(props.planet.name)}>&#10084;</button>
+      </div>
+    </div>
+  ):null
+  props.type ==="starship"?(
+    field = <div class="card" style="width: 18rem;">
+    <img src={`https://starwars-visualguide.com/assets/img/starships/${props.index + 1}.jpg`}
+    onError={(e)=>{
+        e.target.src='https://starwars-visualguide.com/assets/img/placeholder.jpg'
+    }}
+    class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">{props.starship.name}</h5>
+      <p class="card-text">eye color: {props.starship.manufacturer}</p>
+      <p class="card-text">gender: {props.starship.passengers}</p>
+      <p class="card-text">skin color: {props.starship.orbital_period}</p>
+    </div>
+    <div class="card-buttons"> 
+    <Link to={'profile/starships/'+ props.index} class="btn btn-secondary">🥷</Link>
+    <button class="btn btn-secondary" onClick={()=>actions.selectFavorite(props.starship.name)}>&#10084;</button>
+    </div>
+  </div>
+):null
 
 return (
     <>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,19 +8,40 @@ import Card from "../component/card.jsx"
 export const Home = () => {
 	const {store}=useContext(Context)
 	return (
-		<>
+		<div> 
 		<body>
 			<div class="container">
-				<h2 class="row ">charecters</h2>
+				<h2 class="row ">Charecters</h2>
 				<div class="row">
-					{store.people.map((person,index) =>
+					{store.person.map((person,index) =>
 					<Card key={index} person={person}
 					 index={index}
 					 type="person"/>)}
 				</div>
+				<h2 class="row ">Planet</h2>
+				<div class="row">
+					{store.planet.map((planet,index) =>
+					<Card key={index} planet={planet}
+					 index={index}
+					 type="planet"/>)}
+				</div>
+				<h2 class="row ">Vehicle</h2>
+				<div class="row">
+					{store.vehicle.map((vehicle,index) =>
+					<Card key={index} vehicle={vehicle}
+					 index={index}
+					 type="vehicle"/>)}
+				</div>
+				<h2 class="row ">Starship</h2>
+				<div class="row">
+					{store.starship.map((starship,index) =>
+					<Card key={index} starship={starship}
+					 index={index}
+					 type="starship"/>)}
+				</div>
 			</div>
 		</body>
-		</>
+		</div>
 	)
 	
 };
